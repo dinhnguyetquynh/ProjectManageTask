3f2bd43eeba4f43a87ef18fe6f21d446bb0fa03c
LEGIABAO-24102002 <legiabaoaabbcc@gmail.com>
2025-01-18 01:54:17 +0700

    datafaker account

--- src/main/java/datafaker/DataFakerGenerator.java
+++ src/main/java/datafaker/DataFakerGenerator.java
@@ -1,34 +1,52 @@
 package datafaker;
 
+import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Random;
+import java.util.regex.Pattern;
 
+import model.Account;
 import model.Gender;
 import model.User;
 import net.datafaker.Faker;
 
 public class DataFakerGenerator {
-	
+
 	private final Faker faker;
 	private static final Random random = new Random();
-	
-    public DataFakerGenerator() {
-        this.faker = new Faker(new Locale("vi"));
-    }
 
-    // Tạo dữ liệu giả cho người dùng
-    public User generateFakeUsers() {
-        
-       
-            String fullName = faker.name().fullName();
-            String email = faker.internet().emailAddress();
-            Gender gender = Gender.values()[random.nextInt(Gender.values().length)];
-            int age = faker.random().nextInt(18, 40);
-            User u = new User(fullName, gender, age, email, null);
-           
-            return u;
-    }
+	public DataFakerGenerator() {
+		this.faker = new Faker(new Locale("vi"));
+	}
+
+	// Tạo dữ liệu giả cho người dùng
+	public User generateFakeUsers() {
+
+		String fullName = faker.name().fullName();
+		String email = faker.internet().emailAddress();
+		Gender gender = Gender.values()[random.nextInt(Gender.values().length)];
+		int age = faker.random().nextInt(18, 40);
+		User u = new User(fullName, gender, age, email, null);
+
+		return u;
+	}
+
+	// Tạo dữ liệu giả cho Account
+	public Account generateFakerAccounts(User user) {
+		String accountName = normalizeAccountName(user.getName());
+		String password = faker.internet().password();
+		String role = user.getManager() == null ? "Manager" : "Employee";
+		return new Account(0, accountName, password, role, user);
+	}
+	
+	private String normalizeAccountName(String input) {
+	    String normalized = Normalizer.normalize(input, Normalizer.Form.NFD);
+	    String withoutAccents = normalized.replaceAll("[đĐ]", "d").replaceAll("[^\\p{ASCII}]", "");
+	    return withoutAccents.replaceAll("\\s+", "").toLowerCase();
+	}
+	
+
 
 }
--- src/main/java/test/Runner.java
+++ src/main/java/test/Runner.java
@@ -7,6 +7,7 @@ import datafaker.DataFakerGenerator;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import model.Account;
 import model.Gender;
 import model.User;
 import net.datafaker.Faker;
@@ -14,32 +15,43 @@ import net.datafaker.Faker;
 public class Runner {
 	public static void main(String[] args) {
 		EntityManager em = Persistence.createEntityManagerFactory("task").createEntityManager();
-		
+
 		EntityTransaction tr = em.getTransaction();
 		Faker faker = new Faker(new Locale("vi"));
 		DataFakerGenerator datafaker = new DataFakerGenerator();
 		try {
 			tr.begin();
-			//tạo quản lí
+			// tạo quản lí
 			User quanli = datafaker.generateFakeUsers();
 			em.persist(quanli);
 			em.flush();
-		
-			//tạo nhan viên
-			for(int i=0;i<10;i++) {
+			
+			//tạo tài khoản quản lí
+			Account taikhoanquanli = datafaker.generateFakerAccounts(quanli);
+			em.persist(taikhoanquanli);
+
+			// tạo nhan viên
+			for (int i = 0; i < 5; i++) {
 				User nhanvien = datafaker.generateFakeUsers();
 				nhanvien.setManager(quanli);
 				em.persist(nhanvien);
+				em.flush();
+				
+				//tạo tài khoản nhân viên
+				Account taikhoannhanvien = datafaker.generateFakerAccounts(nhanvien);
+				em.persist(taikhoannhanvien);
+
 			}
-			
-			
+
 			tr.commit();
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
 			tr.rollback();
+		} finally {
+			em.close();
 		}
-		
+
 	}
 
 }
